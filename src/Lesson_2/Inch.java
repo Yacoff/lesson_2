331d60d2484c817cff82a3eb648026aa70f1d750
Yacoff <111678812+Yacoff@users.noreply.github.com>
2024-06-28 18:42:06 +0300

    тест 2

---
+++ src/Lesson_2/Inch.java
@@ -0,0 +1,13 @@
+package Lesson_2;
+
+public class Inch {
+    public static void main(String[] args) {
+        double i,c;
+        i = 20;// число в дюймах
+        c = i*0.393700787; // конвертація в сантиметри
+
+
+System.out.println("Сантиметів становить:" +c);
+
+    }
+}
---
+++ src/Lesson_2/Temperature.java
@@ -0,0 +1,13 @@
+package Lesson_2;
+
+public class Temperature {
+    public static void main(String[] args) {
+        double f,c;
+        f = 1;// градуси по Фаренгейту
+        c = ((f-32)*5)/9; //формула конвертації Fr to C
+
+
+System.out.println("Температура в градусах Цельсія становить:" +c);
+
+    }
+}
